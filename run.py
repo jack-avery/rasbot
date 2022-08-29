f4ee9da69c17a32d4fc072b20e4b7fc612be5c98
raspy-on-osu <email.jack.avery@gmail.com>
2022-08-28 21:40:22 -0400

    automate oauth refresh

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import requests
 import click
 from bot import TwitchBot
 from authentication import Authentication
+from definitions import AuthenticationDeniedError
 
 
 @click.command()
@@ -29,9 +30,24 @@ def run(channel=None, auth=None, cfg=None, debug=False):
         channel = auth.get_auth()['user_id']
 
     # Resolve ID from channel name
-    url = f"https://api.twitch.tv/helix/users?login={channel}"
-    r = requests.get(url, headers=auth.get_headers()).json()
-    channel_id = int(f"{r['data'][0]['id']}")
+    channel_id = False
+    while not channel_id:
+        url = f"https://api.twitch.tv/helix/users?login={channel}"
+        r = requests.get(url, headers=auth.get_headers()).json()
+        try:
+            channel_id = int(f"{r['data'][0]['id']}")
+        except KeyError:
+            if r['status'] == 401:
+                print("OAuth key is invalid or expired. Attempting a refresh...")
+                try:
+                    auth.auth['oauth'] = auth.request_oauth()
+                    auth.write_authfile()
+                except AuthenticationDeniedError as err:
+                    print(f"Authentication Denied: {err}")
+                    print("Please ensure that your credentials are valid.")
+                    print("You may need to re-run setup.py.\n")
+                    input("This error is unrecoverable. rasbot will now exit.")
+                    exit(1)
 
     # Start the bot
     tb = TwitchBot(auth, channel_id, channel, cfg, debug)
