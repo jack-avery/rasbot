61c10462ac90ce3c0f66911030bdbb6b8d33164b
raspy-on-osu <email.jack.avery@gmail.com>
2022-09-29 09:17:21 -0400

    remove debug modules

--- modules/debugechofull.py
+++
@@ -1,9 +0,0 @@
-# This is a test method. Feel free to use it as a template.
-
-from commands import BaseModule
-
-
-class Module(BaseModule):
-    # Echo the user's message back to them
-    def main(self, bot):
-        return ' '.join(bot.cmdargs)
