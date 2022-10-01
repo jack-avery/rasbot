658df233966d5015c7c940ea89eb29055bc0cd92
raspy-on-osu <email.jack.avery@gmail.com>
2022-10-01 16:28:23 -0400

    overhaul xp randomness, reward activity

--- modules/xp.py
+++ modules/xp.py
@@ -1,22 +1,20 @@
-# Channel watcher XP code TODO
-
 from commands import BaseModule
 import random
 import requests
 import sqlite3
 import threading
 
-XP_GRANT_FREQUENCY = 60.0
-"""Amount of seconds between each grant."""
+XP_GRANT_FREQUENCY = 180.0
+"""Amount of seconds between each grant. Default is 180 (3 minutes)."""
 
-XP_GRANT_AMOUNT = 10
-"""Amount of XP to grant per grant."""
+XP_GRANT_BASE = 2
+"""Base amount of XP to grant per grant. Default is 2."""
 
-XP_GRANT_VARY = True
-"""Whether the XP granted should vary."""
+XP_GRANT_REWARD_ACTIVITY = True
+"""Whether the XP granted should be increased for active chatters. Default is True."""
 
-XP_GRANT_VARIANCE = 5
-"""How much the XP granted should vary, if it does. (+-)"""
+XP_GRANT_REWARD_AMOUNT = 3
+"""Maximum amount of extra XP to give to active users. Default is 3."""
 
 
 class RepeatTimer(threading.Timer):
@@ -37,11 +35,14 @@ class Module(BaseModule):
         # Create the table if it doesn't exist
         self.db.execute("""
         CREATE TABLE IF NOT EXISTS xp (
-            name,
+            user,
             amt,
-            UNIQUE(name)
+            UNIQUE(user)
         )
         """)
+
+        # Create active users array for activity bonus
+        self.active_users = []
 
         # Tick XP every XP_GRANT_FREQUENCY seconds
         timer = RepeatTimer(XP_GRANT_FREQUENCY, self.tick)
@@ -56,31 +57,32 @@ class Module(BaseModule):
             f"https://tmi.twitch.tv/group/user/{self.bot.channel[1:]}/chatters", headers=self.bot.auth.get_headers()).json()
 
         for utype in users['chatters']:
-            for name in users['chatters'][utype]:
+            for user in users['chatters'][utype]:
                 # Resolve how much XP to grant to this user
-                if XP_GRANT_VARY:
-                    amt = XP_GRANT_AMOUNT + \
-                        random.randint(-XP_GRANT_VARIANCE, XP_GRANT_VARIANCE)
-                else:
-                    amt = XP_GRANT_AMOUNT
+                amt = XP_GRANT_BASE
+                if (user in self.active_users) and XP_GRANT_REWARD_ACTIVITY:
+                    amt += random.randint(1, XP_GRANT_REWARD_AMOUNT)
 
                 # Grant it to the user
-                self.grant_xp(tdb, name, amt)
+                self.grant_xp(tdb, user, amt)
 
-    def grant_xp(self, tdb, name, amt):
-        """Grant `amt` xp to `name`, creating their entry if it doesn't exist.
+        # Clear active users for this window.
+        self.active_users.clear()
+
+    def grant_xp(self, tdb, user, amt):
+        """Grant `amt` xp to `user`, creating their entry if it doesn't exist.
         """
         with tdb as db:
-            db.execute("INSERT OR IGNORE INTO xp VALUES(?,?)", (name, 0))
+            db.execute("INSERT OR IGNORE INTO xp VALUES(?,?)", (user, 0))
             db.execute(
-                f"UPDATE xp SET amt = amt + {amt} WHERE name = \"{name}\"")
+                f"UPDATE xp SET amt = amt + {amt} WHERE user = \"{user}\"")
 
-    def get_xp(self, name) -> int:
-        """Return the amount of xp `name` has, or `0` if the user doesn't exist.
+    def get_xp(self, user) -> int:
+        """Return the amount of xp `user` has, or `0` if the user doesn't exist.
         """
         with self.db as db:
             cs = db.cursor()
-            cs.execute(f"SELECT amt FROM xp WHERE name = \"{name}\"")
+            cs.execute(f"SELECT amt FROM xp WHERE user = \"{user}\"")
             res = cs.fetchone()
             return res[0] if res else 0
 
@@ -93,3 +95,8 @@ class Module(BaseModule):
             user = user[1:]
 
         return f"{user} has {self.get_xp(user)} XP."
+
+    def on_pubmsg(self):
+        # Add this user to the active users list.
+        if not self.bot.author_name in self.active_users:
+            self.active_users.append(self.bot.author_name)
