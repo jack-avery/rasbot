ca365f9c11658a250332d68fdfa9c652db4e75d4
raspy-on-osu <email.jack.avery@gmail.com>
2022-10-03 17:25:36 -0400

    minor cmddel refactor

--- modules/cmddel.py
+++ modules/cmddel.py
@@ -12,16 +12,19 @@ class Module(BaseModule):
     helpmsg = 'Deletes an existing command. Usage: cmddel <name>.'
 
     def main(self):
-        cmd = self.bot.cmdargs
+        if not self.bot.cmdargs:
+            return "No command provided."
+
+        cmd = self.bot.cmdargs[0].lower()
 
         try:
-            self.bot.commands.command_del(cmd[0].lower())
+            self.bot.commands.command_del(cmd)
             config.write(self.bot)
 
-            return f'Command {cmd[0]} removed successfully.'
+            return f'Command {cmd} removed successfully.'
 
         except CommandDoesNotExistError:
-            return f'Command {cmd[0]} does not exist.'
+            return f'Command {cmd} does not exist.'
 
         except CommandGivenInvalidNameError:
             return f'Command name must fit the regular expression {VALID_COMMAND_REGEX}.'
