bd9259840c0a759c84ace3827e6eaa5feaf83a43
raspy-on-osu <email.jack.avery@gmail.com>
2022-12-11 13:56:06 -0500

    refactor to no longer use .txt

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ DEFAULT_CHANNEL = {
 
 DEFAULT_GLOBAL = {
     "always_debug": False,
-    "default_authfile": "auth.txt",
+    "default_authfile": "config/auth",
     "release_branch": "main",
     "channels": [],
 }
@@ -51,7 +51,7 @@ DEFAULT_GLOBAL = {
 def read_global() -> dict:
     """Reads the global config file.
     """
-    return read("rasbot.txt", DEFAULT_GLOBAL)
+    return read("config/rasbot", DEFAULT_GLOBAL)
 
 
 def read_channel(cfg: str) -> dict:
@@ -59,7 +59,8 @@ def read_channel(cfg: str) -> dict:
 
     :param cfgid: The path to the channels' config.
     """
-    return read(f"{cfg}.txt", DEFAULT_CHANNEL)
+    path = f"config/{cfg}"
+    return read(path, DEFAULT_CHANNEL)
 
 
 def read(cfg: str, default: dict) -> dict:
@@ -69,29 +70,28 @@ def read(cfg: str, default: dict) -> dict:
 
     :param default: Default to write to file if the path does not exist.
     """
-    path = f"config/{cfg}"
     # Attempt to read config
     try:
-        with open(path, 'r') as cfgfile:
+        with open(cfg, 'r') as cfgfile:
             config = json.loads(cfgfile.read())
             return config
 
     # If no config file is found, write the default,
     # and return a basic config dict.
     except FileNotFoundError:
-        with open(path, 'w') as cfgfile:
+        with open(cfg, 'w') as cfgfile:
             cfgfile.write(json.dumps(default, indent=4))
             return default
 
 
 def write_channel(bot):
-    """Writes the config file for the given TwitchBot.
+    """Generates then writes the config file for the given TwitchBot.
 
     :param bot: The TwitchBot to write the config for.
     """
     bot.log_debug("config", f"writing for {bot.channel_id}")
 
-    path = f"config/{bot.cfgpath}.txt"
+    path = f"config/{bot.cfgpath}"
 
     # Append prefix to lines
     data = {
@@ -112,5 +112,11 @@ def write_channel(bot):
         }
 
     # Writing config
-    with open(path, 'w') as cfg:
-        cfg.write(json.dumps(data, indent=4))
+    write(path, data)
+
+
+def write(path: str, cfg: dict):
+    """Write `cfg` to `path`.
+    """
+    with open(path, 'w') as file:
+        file.write(json.dumps(cfg, indent=4))
