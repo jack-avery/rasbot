5cc3324c644052d3a0f0a4bc165d450428a2294e
raspy-on-osu <email.jack.avery@gmail.com>
2022-12-11 20:46:23 -0500

    add admin module

---
+++ modules/admin.py
@@ -0,0 +1,22 @@
+# This module contains debug commands mostly used for development purposes.
+# This will likely grow as more cases for admin commands are needed.
+
+from commands import BaseModule
+from definitions import NO_MESSAGE_SIGNAL
+
+
+class Module(BaseModule):
+    def main(self):
+        # only the channel owner can run admin commands
+        if self.bot.author_uid != self.bot.channel_id:
+            return NO_MESSAGE_SIGNAL
+
+        if not self.bot.cmdargs:
+            return NO_MESSAGE_SIGNAL
+
+        args = [a.lower() for a in self.bot.cmdargs]
+
+        match args[0]:
+            case "import":
+                self.bot.commands.module_add(args[1])
+                return f"reimported {args[1]}"
