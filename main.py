8d0dc72a392c160c675d7480e70c3ee8bce4ab6e
raspy-on-osu <email.jack.avery@gmail.com>
2023-05-12 01:06:05 -0400

    report all errors that occur to webhook

--- main.py
+++ main.py
@@ -18,11 +18,13 @@ import click
 import logging
 import os
 import time
+import traceback
 from update import check
 
 from src.config import read_global
 from src.authentication import TwitchOAuth2Helper
 from src.bot import TwitchBot
+from src.telemetry import report_exception
 
 log = logging.getLogger("rasbot")
 formatter = logging.Formatter("%(asctime)s %(levelname)s %(module)s | %(message)s")
@@ -40,56 +42,65 @@ formatter = logging.Formatter("%(asctime)s %(levelname)s %(module)s | %(message)
     default=False,
 )
 def main(channel=None, authfile=None, debug=False):
-    # Check for updates first!
-    check(silent=True)
-
-    # read global config
-    cfg_global = read_global()
-
-    # Set up logging
-    if cfg_global["always_debug"]:
-        debug = True
-
-    loglevel = logging.INFO
-    if debug:
-        loglevel = logging.DEBUG
-
-        if not os.path.exists("logs"):
-            os.mkdir("logs")
-        file_handler = logging.FileHandler(
-            f"logs/{time.asctime().replace(':','-').replace(' ','_')}.log"
-        )
-        file_handler.setLevel(loglevel)
-        file_handler.setFormatter(formatter)
-        log.addHandler(file_handler)
-
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setLevel(loglevel)
-    stdout_handler.setFormatter(formatter)
-    log.addHandler(stdout_handler)
-
-    log.setLevel(loglevel)
-
-    # read auth
-    if not authfile:
-        authfile = cfg_global["default_authfile"]
-
-    auth = TwitchOAuth2Helper(authfile)
-
-    # use self as channel if no channel given
-    if not channel:
-        channel = auth.user_id
-
-    # start the bot
     try:
-        tb = TwitchBot(auth, channel)
-        tb.start()
+        # Check for updates first!
+        check(silent=True)
 
-    # catch ctrl+C and force unimport modules;
-    # speeds up ctrl+C exiting with timed modules
-    except KeyboardInterrupt:
-        tb.__del__()
-        sys.exit(0)
+        # read global config
+        cfg_global = read_global()
+
+        # Set up logging
+        if cfg_global["always_debug"]:
+            debug = True
+
+        loglevel = logging.INFO
+        if debug:
+            loglevel = logging.DEBUG
+
+            if not os.path.exists("logs"):
+                os.mkdir("logs")
+            file_handler = logging.FileHandler(
+                f"logs/{time.asctime().replace(':','-').replace(' ','_')}.log"
+            )
+            file_handler.setLevel(loglevel)
+            file_handler.setFormatter(formatter)
+            log.addHandler(file_handler)
+
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setLevel(loglevel)
+        stdout_handler.setFormatter(formatter)
+        log.addHandler(stdout_handler)
+
+        log.setLevel(loglevel)
+
+        # read auth
+        if not authfile:
+            authfile = cfg_global["default_authfile"]
+
+        auth = TwitchOAuth2Helper(authfile)
+
+        # use self as channel if no channel given
+        if not channel:
+            channel = auth.user_id
+
+        # start the bot
+        try:
+            tb = TwitchBot(auth, channel)
+            tb.start()
+
+        # catch ctrl+C and force unimport modules;
+        # speeds up ctrl+C exiting with timed modules
+        except KeyboardInterrupt:
+            tb.__del__()
+            sys.exit(0)
+
+    except:
+        userid = "Unknown User"
+        if auth:
+            userid = auth.user_id
+
+        # report the exception
+        report_exception(traceback.format_exc(), userid)
 
 
 if __name__ == "__main__":
